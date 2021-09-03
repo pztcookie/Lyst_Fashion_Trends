1e8261adb781ad583b90627adc421ebc422e9a1d
pztcookie <ztpan@ucdavis.edu>
2021-09-03 15:39:51 -0700

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data extraction of lyst fashion trends',
+    author='wissee',
+    license='MIT',
+)
